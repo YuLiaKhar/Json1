b716276373928ab88e122f4911a6b8c2f170de9b
YuLiaKhar <julia.harchenko02@gmail.com>
2021-10-22 19:03:41 +0300

    123Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ Json1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Json1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string file = "Logs/Products.json";
+            if (!File.Exists(file))
+            {
+                File.Create(file);
+            }
+            Product[] array = new Product[5];
+            Console.WriteLine("Введите код, наименование и цену первого товара:");
+            array [0] = new Product (Convert.ToInt32(Console.ReadLine()), Convert.ToString(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Введите код, наименование и цену второго товара:");
+            array [1] = new Product(Convert.ToInt32(Console.ReadLine()), Convert.ToString(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Введите код, наименование и цену третьего товара:");
+            array [2] = new Product(Convert.ToInt32(Console.ReadLine()), Convert.ToString(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Введите код, наименование и цену четвертого товара:");
+            array [3] = new Product(Convert.ToInt32(Console.ReadLine()), Convert.ToString(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Введите код, наименование и цену пятого товара:");
+            array [4] = new Product(Convert.ToInt32(Console.ReadLine()), Convert.ToString(Console.ReadLine()), Convert.ToDouble(Console.ReadLine()));
+
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize(array, options);
+            StreamWriter swproductjson = new StreamWriter(file); // на этой строке выдает ошибку при первом запуске, если файла "Logs/Products.json" изначально нет
+            swproductjson.WriteLine(jsonString);                 // со второго запуска работает нормально
+            swproductjson.Close();
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        //[JsonPropertyName("Код товара")]
+        public int Code { get; set; }
+        //[JsonPropertyName("Наименование товара")]
+        public string Name { get; set; }
+        //[JsonPropertyName("Цена товара")]
+        public double Price { get; set; }
+        public Product(int code, string name, double price)
+        {
+            Code = code;
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ Json2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace Json2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = "Logs/Products.json"; // чтобы работало нужно скопировать созданную в первой программе папку с файлом в новую программу 
+            StreamReader srProductJson = new StreamReader(file);
+            string productJsonReadToEnd = srProductJson.ReadToEnd();
+            Console.WriteLine(productJsonReadToEnd);
+            Product[] products = JsonSerializer.Deserialize<Product[]>(productJsonReadToEnd);
+
+            int n = 0;
+            double max = products[n].Max();
+            string maxName = products[n].MaxName();
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (max < products[i + 1].Max())
+                {
+                    max = products[i + 1].Max();
+                    maxName = products[i + 1].MaxName();
+                }
+            }
+            Console.WriteLine("Самый дорогой товар: {0}. Его стоимость равна {1}.", maxName, max);
+
+            Console.ReadKey();
+        }
+    }
+    class Product
+    {
+        //[JsonPropertyName("Код товара")]
+        public int Code { get; set; }
+        //[JsonPropertyName("Наименование товара")]
+        public string Name { get; set; }
+        //[JsonPropertyName("Цена товара")]
+        public double Price { get; set; }
+        public Product(int code, string name, double price)
+        {
+            Code = code;
+            Name = name;
+            Price = price;
+        }
+        public double Max()
+        {
+            return Price;
+        }
+        public string MaxName()
+        {
+            return Name;
+        }
+    }
+}
